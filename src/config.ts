05604714815e830e36d74f234ce4d0582c24ff39
Daniel Nørgaard <ddnoergaard@gmail.com>
2026-06-05 10:47:37 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-//export const API_BASE = 'https://api.lockmate.dk' //PROD ENV
-export const API_BASE = 'https://localhost:7014' //DEV ENV
+export const API_BASE = 'https://api.lockmate.dk' //PROD ENV
+//export const API_BASE = 'https://localhost:7014' //DEV ENV
